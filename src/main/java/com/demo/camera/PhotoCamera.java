7b07c87a8fecb24e4bc20776f7cca963da2ba396
yesiu2 <woytehu@gmail.com>
2017-11-09 16:50:27 +0100

    Added TODO list and TODO filters

--- src/main/java/com/demo/camera/PhotoCamera.java
+++ src/main/java/com/demo/camera/PhotoCamera.java
@@ -3,15 +3,32 @@ package com.demo.camera;
 public class PhotoCamera {
 
     public void turnOn() {
-        // not implemented
+        // not implemented || turns on the sensor
     }
 
     public void turnOff() {
-        // not implemented
+        // not implemented || turns off the sensor
+        // when camera is writing data, turning off shouldnt turn off the power
+        // when writing data ends, camera cut the power of sensor
     }
 
     public void pressButton() {
-        // not implemented
+        // not implemented || should doing nothing when camera is off ||
+        // when camera is on it should copy data from sensor to memory card
+        // writing data on memory card should take few seconds
+
     }
 }
+
+//    TODO-Klasa Camera
+//
+//    TODO-Camera musi posiadać następujące zachowania:
+//
+//       todo-kuba Włączenie kamery włącza zasilanie sensora.
+//       todo-wojtek Wyłączenie kamery odcina zasilanie sensora.
+//        TODO-Naciśnięcie migawki jeśli zasilanie jest odcięte nie robi nic.
+//        TODO-Naciśnięcie migawki z włączonym zasilaniem, kopiuje dane z sensora do karty pamięci. Zapisywanie danych do karty pamięci może zająć kilka sekund.
+//       TODO- Jeśli dane są obecnie zapisywane, wyłączenie kamery nie odcina zasilania sensora
+//        TODO-Kiedy zapis danych się zakończy, aparat odcina zasilanie sensora.
+
 
--- src/test/java/com/demo/camera/PhotoCameraSpec.groovy
+++ src/test/java/com/demo/camera/PhotoCameraSpec.groovy
@@ -1,11 +1,19 @@
 package com.demo.camera
 
+import javafx.scene.Camera
 import spock.lang.Specification
 
 class PhotoCameraSpec extends Specification {
 
 
     def "Should power up the sensor when camera is switched on"() {
+
+        given:
+        Camera cam = Mock(Camera)
+
+        when:
+
+        then:
 
     }
 
